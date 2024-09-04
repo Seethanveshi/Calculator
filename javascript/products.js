17b49e777d4ad0168b4bdcc8566e5e9cc8572b63
Seethanveshi <84505572+Seethanveshi@users.noreply.github.com>
2024-09-04 20:57:31 +0530

    Add files via upload

---
+++ javascript/products.js
@@ -0,0 +1,50 @@
+export let products = [
+    {
+        id: '1',
+        image: 'Product-imgs/product-img1.jpg',
+        brand: 'RoadStar',
+        name: 'Mens Black Shirt',
+        price_details: {
+            price: 499,
+            actual_price: 998,
+            discount: 50
+        },
+        size: 'L'
+    },
+    {
+        id: '2',
+        image: 'Product-imgs/product-img2.webp',
+        brand: 'RoadStar',
+        name: 'Mens Darkred Shirt',
+        price_details: {
+            price: 499,
+            actual_price: 998,
+            discount: 50
+        },
+        size: 'L'
+    },
+    {
+        id: '3',
+        image: 'Product-imgs/product-img3.jpeg',
+        brand: 'RoadStar',
+        name: 'Mens Black checks Shirt',
+        price_details: {
+            price: 499,
+            actual_price: 998,
+            discount: 50
+        },
+        size: 'L'
+    },
+    {
+        id: '4',
+        image: 'Product-imgs/product-img4.webp',
+        brand: 'RoadStar',
+        name: 'Mens Navyblue Shirt',
+        price_details: {
+            price: 499,
+            actual_price: 998,
+            discount: 50
+        },
+        size: 'L'
+    }
+];
